da39e806b962681c5c09be9935c7658ab5cbd502
Isabella Basso <isabellabdoamaral@usp.br>
2021-09-12 11:36:15 -0300

    update config with ruby stuff
    Signed-off-by: Isabella Basso <isabellabdoamaral@usp.br>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,9 +4,13 @@ export ANDROID_HOME=/opt/android-sdk/
 export default_user=isinyaaa
 fish_vi_key_bindings
 
+#status --is-interactive; and source (rbenv init -|psub)
+
+
 # update path
 set -gx PATH $PATH $HOME/bin
 set -gx PATH $PATH $HOME/.local/bin
+set -gx PATH $PATH $HOME/.local/share/gem/ruby/3.0.0/bin
 set -gx PATH $PATH $ANDROID_HOME/cmdline-tools/latest/bin
 
 export GPG_TTY=(tty)
