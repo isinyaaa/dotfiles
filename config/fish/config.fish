98e8b7fc3d7276c830bd8370d5289437dbb54ccd
Isabella Basso <isabellabdoamaral@usp.br>
2021-09-12 11:39:31 -0300

    refactor fish config file
    Signed-off-by: Isabella Basso <isabellabdoamaral@usp.br>

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,28 +1,35 @@
 #source ~/.fancy-bash-promt.sh
 
-export ANDROID_HOME=/opt/android-sdk/
-export default_user=isinyaaa
-fish_vi_key_bindings
+# add export for working with android
+#export ANDROID_HOME=/opt/android-sdk
+#set -gx PATH $PATH $ANDROID_HOME/cmdline-tools/latest/bin
 
+# for managing keys
+export GPG_TTY=(tty)
+
+# for managing ruby
 #status --is-interactive; and source (rbenv init -|psub)
-
+#set -gx PATH $PATH $HOME/.local/share/gem/ruby/3.0.0/bin
 
 # update path
 set -gx PATH $PATH $HOME/bin
 set -gx PATH $PATH $HOME/.local/bin
-set -gx PATH $PATH $HOME/.local/share/gem/ruby/3.0.0/bin
-set -gx PATH $PATH $ANDROID_HOME/cmdline-tools/latest/bin
+set -gx PATH $PATH $HOME/.cargo/bin
 
-export GPG_TTY=(tty)
+# set user preferences
+export default_user=isinyaaa
+export NVIM_TUI_ENABLE_CURSOR_SHAPE=1
 
-# set the correct timezone for prompt
+set -gx EDITOR nvim
+
+# set the correct timezone and format for prompt
 set -g theme_date_timezone America/Sao_Paulo
 set -g theme_date_format "+%l:%M%P"
 
-# set editor
-set -gx EDITOR nvim
-
 function fish_greeting
-	colorscript --exec spectrum
+    colorscript --exec spectrum
+end
+
+alias wipe="clear; fish_greeting"
 end
 
