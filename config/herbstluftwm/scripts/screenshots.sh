d06822ebcf7d0781d3580bbe10a95904b2dd91ee
Isabella Basso <isabbasso@riseup.net>
2021-10-24 19:38:25 -0300

    herbst: scripts: improve code style

--- config/herbstluftwm/scripts/screenshots.sh
+++ config/herbstluftwm/scripts/screenshots.sh
@@ -2,13 +2,34 @@
 
 maim_args="-u -b 3 -m 5"
 
-if [ $# -gt 0 ]
-then
+function selection()
+{
     filename="/etc/maim_clipboard"
-    maim $maim_args -s /tmp/maim_clipboard && xclip -selection clipboard -t image/png /tmp/maim_clipboard &>/dev/null && rm /tmp/maim_clipboard
-else
+    maim $maim_args -s /tmp/maim_clipboard && xclip \
+        -selection clipboard \
+        -t image/png /tmp/maim_clipboard &>/dev/null && \
+        rm /tmp/maim_clipboard
+}
+
+function fullscreen()
+{
     screenshots_path="$HOME/pictures/screenshots"
     [ -d $screenshots_path ] || mkdir -p $screenshots_path
     filename="$screenshots_path/$(date +%Y.%m.%d-%H.%M.%S)-screenshot.png"
-    maim $maim_args $filename && xclip -selection clipboard -t image/png $filename &>/dev/null
+    maim $maim_args $filename && xclip \
+        -selection clipboard \
+        -t image/png $filename &>/dev/null
+}
+
+if [ "$#" > 0 ]; then
+    case $1 in
+        --selection)
+            selection
+            ;;
+        *)
+            fullscreen
+            ;;
+    esac
+else
+    printf "%s" "no arguments passed, exiting..."
 fi
--- config/herbstluftwm/scripts/xrandr.sh
+++ config/herbstluftwm/scripts/xrandr.sh
@@ -1,21 +1,53 @@
 #!/bin/bash
 
-if [ $1 -eq 1 ]
-then
-    edp_flag="--auto"
-    hdmi_flag="--off"
+dpi=100
+
+xrandr-set()
+{
+    [ $# == 2 ] || return
+    eval xrandr --output eDP1 "$1" --output HDMI1 "$2"
+}
+
+function laptop-only()
+{
+    edp_flags="--auto"
+    hdmi_flags="--off"
     dpi=110
+}
+
+function hdmi-only()
+{
+    edp_flags="--off"
+    hdmi_flags="--auto"
+}
+
+function dual-monitors()
+{
+    # small hack to make it work properly
+    hdmi-only
+    xrandr-set
+    edp_flags="--auto"
+    hdmi_flags="--auto --left-of eDP1 --primary"
+}
+
+if [ "$#" > 0 ]; then
+    case $1 in
+        --laptop-only)
+            laptop-only
+            ;;
+        --hdmi-only)
+            hdmi-only
+            ;;
+        *)
+            dual-monitors
+            ;;
+    esac
+
+    xrandr-set "$edp_flags" "$hdmi_flags"
+
+    echo "Xft.dpi: $dpi" | xrdb -merge
+
+    herbstclient reload
 else
-    edp_flag="--off"
-    hdmi_flag="--auto"
-    dpi=100
+    printf "%s" "no arguments passed, exiting..."
 fi
-
-xrandr --output eDP1 $edp_flag --output HDMI1 $hdmi_flag
-
-[ $1 -eq 3 ] && xrandr --output eDP1 --auto --output HDMI1 --auto --left-of eDP1 --primary
-
-echo "Xft.dpi: $dpi" | xrdb -merge
-
-herbstclient reload
-
