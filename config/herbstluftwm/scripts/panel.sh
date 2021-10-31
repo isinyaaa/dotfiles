b7e1f6c4adea981613722900df3ec3b79d739119
Isabella Basso <isabbasso@riseup.net>
2021-10-31 17:24:59 -0300

    hebst: polybar: update bar config

--- config/herbstluftwm/scripts/panel.sh
+++ config/herbstluftwm/scripts/panel.sh
@@ -1,17 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Terminate already running bar instances
-killall -q polybar
+pgrep polybar >/dev/null && killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 0.05; done
 
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mainbar-herbst &
+BAR=mainbar
+POLYBAR=$(polybar -m)
+
+if [ $(wc -l <<< "$POLYBAR" | cut -d" " -f1) -gt 1 ]; then
+  for m in $(cut -d":" -f1 <<< "$POLYBAR"); do
+    #grep -q "$m.*primary" <<< "$POLYBAR" || BAR=dual
+    grep -q "$m.*primary" <<< "$POLYBAR" && \
+      MONITOR=$m polybar --reload $BAR &
   done
 else
-  polybar --reload mainbar-herbst &
+  MONITOR=$(cut -d":" -f1 <<< "$POLYBAR") polybar --reload $BAR &
 fi
-
-echo "Bars launched..."
