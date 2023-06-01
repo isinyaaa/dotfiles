015cdc03a7790f23ed06ba1541743abf69ba61c3
Isabella Basso <isabellabdoamaral@usp.br>
2023-05-31 21:34:55 -0300

    fish: move export from eseal

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -65,6 +65,8 @@ if command -q pyenv
     set -gx PYENV_ROOT $HOME/.pyenv
     set -gx PATH $PYENV_ROOT/bin $PATH
 
+    set -g VIRTUAL_ENV_DISABLE_PROMPT 1
+
     status is-login
     and pyenv init --path | source
 
