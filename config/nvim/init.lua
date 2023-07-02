248355dc24a8ebd85eadb950626d425d7c8938c8
Isabella Basso <isabellabdoamaral@usp.br>
2023-07-02 13:16:17 -0300

    nvim: setup neoscroll

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -115,8 +115,29 @@ vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+require('neoscroll').setup({
+    easing_function = "quadratic" -- Default easing function
+    -- Set any other options as needed
+})
+
+-- Syntax: [key] = {function, {function arguments}}
+local t = {
+  ['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '350'}},
+  ['<C-d>'] = {'scroll', {'vim.wo.scroll', 'true', '350'}},
+  ['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '500'}},
+  ['<C-f>'] = {'scroll', {'vim.api.nvim_win_get_height(0)', 'true', '500'}},
+  ['<C-y>'] = {'scroll', {'-0.10', 'false', '100'}},
+  ['<C-e>'] = {'scroll', {'0.10', 'false', '100'}},
+  ['zt'] = {'zt', {'300'}},
+  ['zz'] = {'zz', {'300'}},
+  ['zb'] = {'zb', {'300'}},
+}
+
+require('neoscroll.config').set_mappings(t)
+
+-- Those don't work with neoscroll
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true, noremap = false })
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- vim.keymap.set("n", "n", "nzzzv")
 -- vim.keymap.set("n", "N", "Nzzzv")
