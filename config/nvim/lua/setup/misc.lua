82cd903ee4693158d062c1f4952123b4725379a6
Isabella Basso <isabellabdoamaral@usp.br>
2023-09-22 11:26:05 -0300

    nvim/lua: add descriptive comments

--- config/nvim/lua/setup/misc.lua
+++ config/nvim/lua/setup/misc.lua
@@ -1,7 +1,10 @@
+-- get rid of trailing whitespace
 vim.keymap.set("n", "<leader>hw", [[:%s/\s\+$//e<CR>]])
+
+-- fix text formatting on html files
 vim.keymap.set("n", "<leader>ml", [[:set nowrapscan<CR>/[\.?!>]$<CR>jV/^\s*[A-Z<]<CR>kgq<Esc>:noh<CR>:set wrapscan<CR>]])
 
--- create a map to run %s/:\([^:]\+\):/\=emoji#for(submatch(1), submatch(0))/g
+-- substitute emoji codes with actual emoji
 vim.keymap.set("n", "<leader>re", [[:%s/:\([^:]\+\):/\=emoji#for(submatch(1), submatch(0))/g<CR>]])
 
 vim.api.nvim_create_autocmd({"BufNewFile", "BufRead"}, {
