27c7d05b3c003e17bed9c0f1ec7dde7f465c221f
Isabella Basso <isabellabdoamaral@usp.br>
2023-09-30 11:14:09 -0300

    fish: get rid of node@16

--- config/fish/conf.d/macos.fish
+++ config/fish/conf.d/macos.fish
@@ -1,10 +1,7 @@
 function __setup_macos
-    set -gx PATH /opt/homebrew/opt/python@3.10/bin $PATH # python 3.10
+    set -gx PATH /opt/homebrew/opt/python@3.11/bin $PATH # python 3.11
     set -gx PATH $HOME/.gem/ruby/2.6.0/bin $PATH # ruby gems
     set -gx PATH $HOME/go/bin $PATH # go
-
-    # copilot needs node@16
-    set -gx PATH /opt/homebrew/opt/node@16/bin $PATH
 
     # locale variables
     set -gx LC_CTYPE en_US.UTF-8
