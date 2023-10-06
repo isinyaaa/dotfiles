5240c2afc3e4e4917eb6feef7dc37cf2dac55c58
Isabella Basso <isabellabdoamaral@usp.br>
2023-10-06 11:00:09 -0300

    fish: setup java on mac

--- config/fish/conf.d/macos.fish
+++ config/fish/conf.d/macos.fish
@@ -2,6 +2,7 @@ function __setup_macos
     set -gx PATH /opt/homebrew/opt/python@3.11/bin $PATH # python 3.11
     set -gx PATH $HOME/.gem/ruby/2.6.0/bin $PATH # ruby gems
     set -gx PATH $HOME/go/bin $PATH # go
+    export JAVA_HOME=(brew --prefix java)
 
     # locale variables
     set -gx LC_CTYPE en_US.UTF-8
