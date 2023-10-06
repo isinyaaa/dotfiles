701ef93b4766019c123451310c8c6c1022a70d85
Isabella Basso <isabellabdoamaral@usp.br>
2023-10-06 11:00:09 -0300

    fish: set PATH before additional setup

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,18 +1,11 @@
 set -gx IS_MAC false
 # find out if we're on mac
-if uname -s | grep -iq 'darwin'
+if uname -s | grep -iq darwin
     # we redefine the OSTYPE to be the same as any UNIX system
     # because MacOS doesn't have it
     set -gx OSTYPE (uname -s | tr -s '[:upper:]' '[:lower:]')(uname -r)
     set -gx IS_MAC true
 end
-
-# set user preferences
-command -q bat
-and set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
-command -q nvim
-and set -gx EDITOR (which nvim)
-and set -gx VISUAL (which nvim)
 
 # refresh sudo timeout
 alias sudo 'command sudo -v; command sudo '
@@ -26,14 +19,30 @@ set -g theme_date_format "+%l:%M%p"
 set -gx GPG_TTY (tty)
 
 # update path
-set -gx PATH $HOME/bin $PATH
-set -gx PATH $HOME/.local/bin $PATH
-set -gx PATH $HOME/.cargo/bin $PATH # rust
 set -gx PATH /opt/local/bin $PATH
+set -gx PATH $HOME/.cargo/bin $PATH
+set -gx PATH $HOME/.local/bin $PATH
+set -gx PATH $HOME/bin $PATH
+
+# system specific setup
+if test $IS_MAC = true
+    __setup_macos
+else
+    __setup_linux
+end
+
+command -q rye
 set -gx PATH $HOME/.rye/shims $PATH
 
+# set user preferences
+command -q bat
+and set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+command -q nvim
+and set -gx EDITOR (which nvim)
+and set -gx VISUAL (which nvim)
+
 # kitty ssh setup
-echo "$TERM" | grep -q "kitty"
+echo "$TERM" | grep -q kitty
 and test -z "$SSH_CLIENT"
 and alias ssh "kitty +kitten ssh"
 
@@ -41,13 +50,6 @@ and alias ssh "kitty +kitten ssh"
 function fish_greeting
     command -q colorscript
     and colorscript --exec spectrum
-end
-
-# system specific setup
-if test $IS_MAC = true
-    __setup_macos
-else
-    __setup_linux
 end
 
 # setup commands
