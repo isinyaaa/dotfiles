6761de5ad5f72119655b7e4144ea7db7033b0221
Isabella Basso <isabellabdoamaral@usp.br>
2023-10-06 11:00:09 -0300

    nvim: add refactoring plugin

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -24,6 +24,7 @@ return {
     },
     {
         "zbirenbaum/copilot-cmp",
+        optional = true,
         dependencies = {
             "zbirenbaum/copilot.lua",
         },
@@ -46,8 +47,31 @@ return {
             }))
         end,
     },
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("refactoring").setup()
+
+            vim.keymap.set("x", "<leader>re", ":Refactor extract ")
+            vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
+
+            vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
+
+            vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+
+            vim.keymap.set("n", "<leader>rI", ":Refactor inline_func")
+
+            vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
+            vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
+        end,
+    },
+    -- { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.docker" },
+    -- { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
