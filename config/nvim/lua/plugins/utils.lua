93c228f1c0c17276d6ce7069ceffa7a04154ffdb
Isabella Basso <isabellabdoamaral@usp.br>
2023-12-11 20:42:48 -0300

    nvim: add keybind to go to a yaml ref

--- config/nvim/lua/plugins/utils.lua
+++ config/nvim/lua/plugins/utils.lua
@@ -6,4 +6,10 @@ return {
     "blankname/vim-fish",
     "tpope/vim-fugitive",
     "junegunn/goyo.vim",
+    {
+        "Einenlum/yaml-revealer",
+        config = function()
+            vim.keymap.set("n", "<leader>ygd", 'T#vt":s;/;>;g<CR>:noh<CR>gvyu:call SearchYamlKey()<CR><C-r>+<CR>')
+        end,
+    },
 }
